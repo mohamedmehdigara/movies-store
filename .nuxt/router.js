b35839e9007c35f41ad7a38475e34b0b7414e525
mohamedmehdigara <garamehdi57@yahoo.com>
2023-03-28 07:57:04 +0100

    added files

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,6 +4,7 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _ef75f800 = () => interopDefault(import('..\\pages\\product.vue' /* webpackChunkName: "pages/product" */))
 const _7d3b3eba = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
@@ -18,6 +19,10 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/product",
+    component: _ef75f800,
+    name: "product"
+  }, {
     path: "/",
     component: _7d3b3eba,
     name: "index"
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
+
+
 <template>
+
+<Fragment>
     <div>Welcome to nuxt.js!</div>
+</Fragment>
+<Fragment>   
+    <nuxt-link to="/product">Product</nuxt-link>
+</Fragment>
   </template>
+
+<script>
+import Fragment from 'vue-fragment';
+Vue.use(Fragment.Plugin);
+</script>
